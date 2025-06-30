b03fa9e8b8c7d86a04511dc4a6971531aa192122
reut <reutdevops@gmail.com>
2025-06-30 23:44:45 +0300

    שינויים חדשים ב-feature

--- models/product.js
+++ models/product.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   description: String,
   category: String,
   price: Number,
   image: String,
+  concepts: [String], // כדי לסנן לפי קונספטים
+  events: [String],   // כדי לסנן לפי אירועים
 });
 
 const Product = mongoose.model('Product', productSchema);
